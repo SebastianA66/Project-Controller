e05988d75ad444587f1c6299135aee2283c97a36
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-09-12 01:03:14 +1000

    Initial commit

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject attack;
+
+    public float timer;
+
+    void Start()
+    {
+        attack.SetActive(false);  
+    }
+
+    void Update()
+    {
+        timer += 1.0f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            attack.SetActive(true);
+            if(timer >= 1)
+            {
+                GameObject.Destroy(attack);
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CTRL + K + D
+
+public class Controller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 10f;
+    public Rigidbody rigid;
+    public float rayDistance = 1f;
+
+    private void OnDrawGizmos()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * rayDistance);
+    }
+
+    private bool isGrounded = true;
+
+    
+
+    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    bool IsGrounded()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, out hit, rayDistance))
+        {
+            return true; //is grounded
+        }
+        return false; // is not grounded
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal") * moveSpeed;
+        float inputV = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 moveDir = new Vector3(inputH, 0f, inputV);
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            force.y = jumpHeight;
+        }
+
+        rigid.velocity = force;
+
+        if (moveDir.magnitude > 0)
+        {
+            // Rotate the player to that moveDir
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+    }
+
+}
+
